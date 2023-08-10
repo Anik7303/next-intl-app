e240bef2eb3454142844b81f7e3f8c9b20811519
Anik Mohammad <anik7303@gmail.com>
2023-08-11 00:51:08 +0600

    disable `accept-language` header check to determine `locale`

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,9 @@
 import createMiddleware from "next-intl/middleware";
 
 export default createMiddleware({
-  // a list of locales that are supported
-  locales: ["en", "bn"],
-  // if this locale is matched, pathnames work without a prefix (e.g. `/about`)
-  defaultLocale: "bn",
+  locales: ["en", "bn"], // a list of locales that are supported
+  defaultLocale: "bn", // if this locale is matched, pathnames work without a prefix (e.g. `/about`)
+  localeDetection: false, // don't use `accept-language` header to determine `locale`
 });
 
 export const config = {
